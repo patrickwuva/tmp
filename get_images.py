dcd02971e7743fdbf2cefa05919168ba997f62cc
Patrick Williamson <upm8pb@virginia.edu>
2024-08-28 13:57:34 -0400

    updating

--- get_images.py
+++ get_images.py
@@ -4,6 +4,8 @@ import io
 from based import based
 import os
 import glob
+import threading
+import requests
 
 def upload(bucket_name, file_name, destination):
     client = storage.Client()
@@ -12,15 +14,25 @@ def upload(bucket_name, file_name, destination):
     blob.upload_from_filename(file_name)
     print(f'Image uploaded to {blob_name}')
 
+
+def download(link):
+    name = link.split('/')[-1]
+    response = requests.get(link)
+    if response.status_code == 200:
+        with open(f'images/{name}', 'wb') as file:
+            file.write(response.content)
+    else:
+        print(f'failed to download {link}, code: {response.status_code}')
+
 def get_images(links):
+    threads = []
     for link in links:
-        name = link.split('/')[-1]
-        response = requests.get(link)
-        if response.status_code == 200:
-            with open(f'images/{name}', 'wb') as file:
-                file.write(response.content)
-        else:
-            print(f'failed to download {link} statuscode: {response.status_code}')
+        thread = threading.Thread(target_download, args=(link,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 def process_images():
     for file in glob.glob('images/*'):
