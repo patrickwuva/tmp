0a8b3fafb0827461828b42ce4a012fb01a4bbeec
Patrick Williamson <upm8pb@virginia.edu>
2024-09-04 14:20:14 -0400

    updating

---
+++ test.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+from get_images import get_images
+
+sqs = boto3.resource('sqs', region_name='us-east-1')
+
+def send_ack():
+    queue = sqs.get_queue_by_name(QueueName='ack')
+    response = queue.send_message(MessageBody='done')
+
+def get_images():
+    tasks = sqs.get_queue_by-name(QueueName='tasks')
+
+    messages = queue.receive_messages(
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=2
+    )
+
+    if messages:
+        for m in messages:
+            links = json.loads(m.body())
+            try:
+                get_images(links)
+                send_ack()
+                m.delete()
+            except Exception as e:
+                print(f'error: {e}')
+
+while True:
+    get_images()
+
